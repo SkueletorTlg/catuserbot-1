90c02e5897ad77ce97d838a32c215ba9e8d50cbc
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-17 00:42:53 -0400

    Update ocr.py

--- userbot/plugins/ocr.py
+++ userbot/plugins/ocr.py
@@ -44,7 +44,7 @@ async def ocr_space_file(
 @bot.on(admin_cmd(pattern="ocr(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="ocr(?: |$)(.*)", allow_sudo=True))
 async def ocr(event):
-    catevent = await edit_or_reply(event, "`Reading...`")
+    catevent = await edit_or_reply(event, "`Leyendo...`")
     if not os.path.isdir(Config.TEMP_DIR):
         os.makedirs(Config.TEMP_DIR)
     lang_code = event.pattern_match.group(1)
@@ -55,9 +55,9 @@ async def ocr(event):
     try:
         ParsedText = test_file["ParsedResults"][0]["ParsedText"]
     except BaseException:
-        await catevent.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
+        await catevent.edit("`Lo siento, no puedo leer eso...`\n`Supongo que necesito lentes nuevos.`")
     else:
-        await catevent.edit(f"`Here's what I could read from it:`\n\n{ParsedText}")
+        await catevent.edit(f"`Esto es lo que pude leer de esa foto:`\n\n{ParsedText}")
     os.remove(downloaded_file_name)
 
 
